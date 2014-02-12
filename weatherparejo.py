aa9304ece2df94be81b636e6339ce2d638bcc03e
JoseluisParejo <joseluisparejofernandez@gmail.com>
2014-02-12 15:03:33 +0100

    Update weatherparejo.py
    Cambio de algunas lineas y tildes

--- weatherparejo.py
+++ weatherparejo.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
+# -*- coding utf-8 -*-
 import requests
 import json
 
-ciudades = {"1":"Sevilla","2":"Málaga","3":"Cádiz","4":"Córdoba","5":"Huelva","6":"Granada","7":"Jaén","8":"Almería"}
+ciudades = {"1":"Sevilla","2":"Malaga","3":"Cadiz","4":"Cordoba","5":"Huelva","6":"Granada","7":"Jaen","8":"Almeria"}
 
 print 
 """
 1. Sevilla
-2. Málaga
-3. Cádiz
-4. Córdoba
+2. Malaga
+3. Cadiz
+4. Cordoba
 5. Huelva
 6. Granada
-7. Jaén
-8. Almería
+7. Jaen
+8. Almeria
 """
-peti = raw_input("¿De que ciudad quieres conoces el tiempo hoy?: ")
+peti = raw_input("De que ciudad quieres conoces el tiempo hoy?: ")
 respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % ciudades[peti]})
 dicci = json.loads(respuesta.text)
 tempe = dicci["main"]["temp"] - 273
